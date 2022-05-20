9450719f2cafe88bdfd60c8c90497609671e7f2e
63o15o3Apiwat Seesaeng <jobnewap06@gmail.com>
2022-05-21 00:31:59 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Navbar from './Navbar';
+import Users from './Users';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Users />
+    </div>
+  );
+}
+
+export default App;
